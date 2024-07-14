0366b22f0010e2bf97fbad84bcfb79dcd075febb
Jessie P. <ritime275@gmail.com>
2024-07-14 01:44:11 -0400

    'sync javascript/index.js to file index.html'

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,9 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment();
 
+losAngelesDateElement.innerHtml = losAngelesTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHtml = `${losAngelesTime.format(
+  "h:mm:ss"
+)} <small>${losAngelesTime.format("A")}</small>`;
