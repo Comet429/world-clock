1a83f8d85d92c642314fcadd0e5d6af6c937f571
Jessie P. <ritime275@gmail.com>
2024-07-14 04:15:10 -0400

    Added moment js cdn to project current date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,22 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment();
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHtml = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHtml = losAngelesTime.format(
-  "h:mm:ss [<small>A</small>"
-);
+  losAngelesDateElement.innerHtml = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHtml = losAngelesTime.format(
+    "h:mm:ss [<small>A</small>"
+  );
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHtml = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHtml = tokyoTime.format("h:mm:ss [<small>A</small>");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
